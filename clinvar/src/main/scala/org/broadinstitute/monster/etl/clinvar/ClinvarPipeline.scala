d7b379ed97238d1b14a67ee5cd845744d0e57e1e
Dan Moran <danmoran@broadinstitute.org>
2019-09-20 14:58:16 -0400

    DSPDC-497 Basic structure and linking between tables.

--- clinvar/src/main/scala/org/broadinstitute/monster/etl/clinvar/ClinvarPipeline.scala
+++ clinvar/src/main/scala/org/broadinstitute/monster/etl/clinvar/ClinvarPipeline.scala
@@ -1,7 +1,6 @@
 package org.broadinstitute.monster.etl.clinvar
 
 import caseapp.{AppName, AppVersion, HelpMessage, ProgName}
-import cats.data.NonEmptyList
 import com.spotify.scio.ContextAndArgs
 import com.spotify.scio.coders.Coder
 import com.spotify.scio.values.{SCollection, SideOutput}
@@ -35,100 +34,7 @@ object ClinvarPipeline {
         s"${parsedArgs.inputPrefix}/VariationArchive/*.json"
       )
 
-    val (vcvs, List(scvs, rcvs, variations)) = split(
-      // VCV fields
-      Set(
-        NonEmptyList.of("@Accession"),
-        NonEmptyList.of("@Version"),
-        NonEmptyList.of("@DateCreated"),
-        NonEmptyList.of("@DateLastUpdated"),
-        NonEmptyList.of("@NumberOfSubmissions"),
-        NonEmptyList.of("@NumberOfSubmitters"),
-        NonEmptyList.of("RecordStatus"),
-        NonEmptyList.of("InterpretedRecord", "ReviewStatus"),
-        NonEmptyList.of("Species"),
-        NonEmptyList.of("@ReleaseDate")
-      ),
-      // SCV fields (need further splitting)
-      Set(
-        NonEmptyList
-          .of("InterpretedRecord", "ClinicalAssertionList", "ClinicalAssertion"),
-        NonEmptyList.of("IncludedRecord", "ClinicalAssertionList", "ClinicalAssertion")
-      ),
-      // RCV fields
-      Set(
-        NonEmptyList.of("InterpretedRecord", "RCVList", "RCVAccession"),
-        NonEmptyList.of(
-          "InterpretedRecord",
-          "ClinicalAssertionList",
-          "ClinicalAssertion",
-          "ObservedInList",
-          "ObservedIn",
-          "ObservedData"
-        ),
-        NonEmptyList.of("IncludedRecord", "RCVList", "RCVAccession"),
-        NonEmptyList.of(
-          "IncludedRecord",
-          "ClinicalAssertionList",
-          "ClinicalAssertion",
-          "ObservedInList",
-          "ObservedIn",
-          "ObservedData"
-        )
-      ),
-      // Variation fields
-      Set(
-        NonEmptyList.of("InterpretedRecord", "SimpleAllele"),
-        NonEmptyList.of("InterpretedRecord", "Haplotype"),
-        NonEmptyList.of("InterpretedRecord", "Genotype"),
-        NonEmptyList.of("IncludedRecord", "SimpleAllele"),
-        NonEmptyList.of("IncludedRecord", "Haplotype"),
-        NonEmptyList.of("IncludedRecord", "Genotype")
-      )
-    )(fullArchives)
-
-    val (assertions, List(submitters, submissions, assertionVariations)) = split(
-      // Clinical Assertion fields
-      Set(
-        NonEmptyList.of("@ID"),
-        NonEmptyList.of("ClinVarAccession", "@Accession"),
-        NonEmptyList.of("ClinVarAccession", "@Version"),
-        NonEmptyList.of("ClinVarAccession", "@title"),
-        NonEmptyList.of("ClinVarAccession", "@localKey"),
-        NonEmptyList.of("ClinVarAccession", "@Type"),
-        NonEmptyList.of("@DateCreated"),
-        NonEmptyList.of("@DateLastUpdated"),
-        NonEmptyList.of("ClinVarSubmissionID", "@submittedAssembly"),
-        NonEmptyList.of("RecordStatus"),
-        NonEmptyList.of("ReviewStatus"),
-        NonEmptyList.of("Interpretation", "Description"),
-        NonEmptyList.of("Interpretation", "@DateLastEvaluated"),
-        NonEmptyList.of("Interpretation", "Comment", "$"),
-        NonEmptyList.of("Interpretation", "Comment", "@Type")
-      ),
-      // Submitter fields
-      Set(
-        NonEmptyList.of("ClinVarAccession", "@OrgID"),
-        NonEmptyList.of("ClinVarAccession", "@SubmitterName"),
-        NonEmptyList.of("ClinVarAccession", "@OrganizationCategory"),
-        NonEmptyList.of("ClinVarAccession", "@OrgAbbreviation")
-      ),
-      // Submission fields
-      Set(
-        NonEmptyList.of("ClinVarAccession", "@OrgID"),
-        NonEmptyList.of("@SubmissionDate"),
-        NonEmptyList.of("ClinVarAccession", "@SubmitterName")
-      ),
-      // Clinical Assertion Variation fields
-      Set(
-        NonEmptyList.of("SimpleAllele", "@ID"),
-        NonEmptyList.of("SimpleAllele", "VariantType"),
-        NonEmptyList.of("Haplotype", "@ID"),
-        NonEmptyList.of("Haplotype", "VariantType"),
-        NonEmptyList.of("Genotype", "@ID"),
-        NonEmptyList.of("Genotype", "VariantType")
-      )
-    )(scvs)
+    val (vcvs, rcvs, variations, scvs, scvVariations) = splitTables(fullArchives)
 
     MsgIO.writeJsonLists(
       vcvs,
@@ -136,33 +42,23 @@ object ClinvarPipeline {
       s"${parsedArgs.outputPrefix}/variation_archive"
     )
     MsgIO.writeJsonLists(
-      assertions,
-      "SCV Clinical Assertion",
-      s"${parsedArgs.outputPrefix}/clinical_assertion"
+      rcvs,
+      "RCV Accession",
+      s"${parsedArgs.outputPrefix}/rcv_accession"
     )
     MsgIO.writeJsonLists(
-      submitters,
-      "SCV Submitter",
-      s"${parsedArgs.outputPrefix}/submitter"
+      scvs,
+      "SCV Combined (Clinical Assertion + Submission + Submitter)",
+      s"${parsedArgs.outputPrefix}/scv_tmp"
     )
     MsgIO.writeJsonLists(
-      submissions,
-      "SCV Submission",
-      s"${parsedArgs.outputPrefix}/submission"
-    )
-    MsgIO.writeJsonLists(
-      assertionVariations,
+      scvVariations,
       "SCV Clinical Assertion Variation",
       s"${parsedArgs.outputPrefix}/clinical_assertion_variation"
     )
     MsgIO.writeJsonLists(
-      rcvs,
-      "RCV",
-      s"${parsedArgs.outputPrefix}/rcv_accession"
-    )
-    MsgIO.writeJsonLists(
       variations,
-      "Variation",
+      "Variation Archive Variation",
       s"${parsedArgs.outputPrefix}/variation_archive_variation"
     )
 
@@ -170,113 +66,140 @@ object ClinvarPipeline {
     ()
   }
 
-  /**
-    * "Drill" into a upack message by descending through a chain of field names,
-    * returning the value(s) found at the end of the chain.
-    *
-    * Arrays found along the chain will be kept as arrays, and further drilling
-    * will be applied to each element in the collection.
-    */
-  def drill(fields: List[String])(msg: Msg): Option[Msg] =
-    fields match {
-      case Nil => Some(msg)
-      case f :: fs =>
-        msg match {
-          case Obj(props) =>
-            props.get(Str(f)).flatMap(drill(fs))
-          case Arr(ms) =>
-            val expanded = ms.map(drill(fs)).collect {
-              // TODO: What if `v` is an array? Should we flatten?
-              case Some(v) => v
+  def splitTables(fullVcvStream: SCollection[Msg]): (
+    SCollection[Msg],
+    SCollection[Msg],
+    SCollection[Msg],
+    SCollection[Msg],
+    SCollection[Msg]
+  ) = {
+    val rcvOut = SideOutput[Msg]
+    val variationOut = SideOutput[Msg]
+    val scvOut = SideOutput[Msg]
+    val scvVariationOut = SideOutput[Msg]
+
+    val (trimmedVcvStream, sideCtx) = fullVcvStream
+      .withSideOutputs(
+        rcvOut,
+        variationOut,
+        scvOut,
+        scvVariationOut
+      )
+      .withName("Split Variation Archives")
+      .map { (fullVcv, ctx) =>
+        val idKey = Str("id")
+
+        // Init top-level VCV info.
+        val vcvObj = fullVcv.obj
+        val vcvId =
+          Str(s"${vcvObj(Str("@Accession")).str}.${vcvObj(Str("@Version")).str}")
+        val trimmedOut = new mutable.LinkedHashMap[Msg, Msg]
+        trimmedOut.update(idKey, vcvId)
+
+        // Pull out top-level record.
+        // Fields which are destined for other tables will be stripped away.
+        // Whatever's left behind will be re-added to the trimmed VCV to
+        // preserve all information.
+        val record =
+          vcvObj.get(Str("InterpretedRecord")).orElse(vcvObj.get(Str("IncludedRecord")))
+        val isInterpreted = vcvObj.contains(Str("InterpretedRecord"))
+
+        record.foreach { originalRecord =>
+          val recordCopy = upack.copy(originalRecord).obj
+
+          // Extract and push out any RCVs in the VCV, tracking the collected IDs.
+          val rcvIds = new mutable.ArrayBuffer[Msg]()
+          val rcvs = for {
+            wrapper <- recordCopy.remove(Str("RCVList"))
+            arrayOrSingle <- wrapper.obj.get(Str("RCVAccession"))
+          } yield {
+            arrayOrSingle match {
+              case Arr(msgs) => msgs
+              case msg       => Iterable(msg)
             }
-            Some(Arr(expanded))
-          case _ => None
+          }
+          rcvs.getOrElse(Iterable.empty).foreach { rcv =>
+            val rcvObj = rcv.obj
+
+            // Generate and track RCV ID.
+            val id =
+              Str(s"${rcvObj(Str("@Accession")).str}.${rcvObj(Str("@Version")).str}")
+            rcvObj.update(idKey, id)
+            rcvIds.append(id)
+
+            // Link back to VCV.
+            rcvObj.update(Str("variation_archive_id"), vcvId)
+
+            // Push to side output.
+            ctx.output(rcvOut, rcv)
+          }
+
+          // Extract and push archive-level variation records.
+          // Variations are still nested when pushed out here.
+          val variation = recordCopy
+            .remove(Str("SimpleAllele"))
+            .orElse(recordCopy.remove(Str("Haplotype")))
+            .orElse(recordCopy.remove(Str("Genotype")))
+
+          val variationId = variation.flatMap(_.obj.get(Str("@VariationID")))
+          variation.foreach { variant =>
+            val vObj = variant.obj
+            // Link the variant back to the VCV and associated RCVs.
+            vObj.update(Str("variation_archive_id"), vcvId)
+            vObj.update(Str("rcv_accession_ids"), Arr(rcvIds))
+
+            // Push to side output.
+            ctx.output(variationOut, variant)
+          }
+
+          // Extract and push out any SCVs and SCV-level variations.
+          val scvs = for {
+            wrapper <- recordCopy.remove(Str("ClinicalAssertionList"))
+            arrayOrSingle <- wrapper.obj.get(Str("ClinicalAssertion"))
+          } yield {
+            arrayOrSingle match {
+              case Arr(msgs) => msgs
+              case msg       => Iterable(msg)
+            }
+          }
+          scvs.getOrElse(Iterable.empty).foreach { scv =>
+            val scvObj = scv.obj
+            val scvId = scvObj(Str("@ID"))
+
+            // Link the SCV back to the VCV, RCVs, and top-level variant.
+            scvObj.update(Str("variation_archive_id"), vcvId)
+            scvObj.update(Str("rcv_accession_ids"), Arr(rcvIds))
+            variationId.foreach(scvObj.update(Str("variation_archive_variation_id"), _))
+
+            // Extract out any SCV-level variation.
+            val variation = scvObj
+              .remove(Str("SimpleAllele"))
+              .orElse(scvObj.remove(Str("Haplotype")))
+              .orElse(scvObj.remove(Str("Genotype")))
+            variation.foreach { variant =>
+              // Link the variation back to its SCV.
+              variant.obj.update(Str("clinical_assertion_id"), scvId)
+              // Push to side output.
+              ctx.output(scvVariationOut, variant)
+            }
+
+            // Push SCV to side output.
+            ctx.output(scvOut, scv)
+          }
+
+          val recordKey = if (isInterpreted) "InterpretedRecord" else "IncludedRecord"
+          trimmedOut.update(Str(recordKey), Obj(recordCopy))
         }
-    }
 
-  /**
-    * Split the fields of objects in a stream into multiple output streams.
-    *
-    * @param mainFields fields to keep in the "main" stream of outputs, which
-    *                   produces one output per message in the input
-    * @param splitFields sets of fields to split into separate streams. Split streams
-    *                    can produce multiple outputs per message in the input
-    */
-  def split(
-    mainFields: Set[NonEmptyList[String]],
-    splitFields: Set[NonEmptyList[String]]*
-  ): SCollection[Msg] => (SCollection[Msg], List[SCollection[Msg]]) = { inStream =>
-    val sideOuts = List.fill(splitFields.length)(SideOutput[Msg])
-    val zipped = splitFields.zip(sideOuts)
-
-    val (mainOuts, side) = inStream.withSideOutputs(sideOuts: _*).map { (msg, ctx) =>
-      val mainOut = new mutable.LinkedHashMap[Msg, Msg]()
-
-      msg.obj.foreach {
-        case (Str(k), v) =>
-          mainFields.find(_.head == k).foreach { fields =>
-            val newName = (fields.head :: fields.tail).mkString(".")
-            drill(fields.tail)(v).foreach(mainOut.update(Str(newName), _))
-          }
-        case _ => ()
+        Obj(trimmedOut): Msg
       }
 
-      zipped.foreach {
-        case (splitFields, sideOut) =>
-          msg.obj.foreach {
-            case (Str(k), v) =>
-              splitFields.find(_.head == k).foreach { fields =>
-                // FIXME: This is broken. We need a way to collect many "paths"
-                // through the data in a single output.
-                drill(fields.tail)(v).foreach {
-                  case Arr(vs) => vs.foreach(ctx.output(sideOut, _))
-                  case v       => ctx.output(sideOut, v)
-                }
-              }
-            case _ => ()
-          }
-      }
-
-      Obj(mainOut): Msg
-    }
-
-    (mainOuts, sideOuts.map(side(_)))
-  }
-
-  def split(
-    rhsFields: Set[String],
-    idLink: (String, String)
-  ): SCollection[Msg] => (SCollection[Msg], SCollection[Msg]) = { inStream =>
-    val sideOut = SideOutput[Msg]
-
-    val (rhs, sideOuts) = inStream.withSideOutputs(sideOut).flatMap { (msg, ctx) =>
-      val (lhsKey, rhsKey) = idLink
-      val lhsId = msg.obj(Str(lhsKey))
-
-      val lhsOut = new mutable.LinkedHashMap[Msg, Msg]()
-      val rhsOut = new mutable.ArrayBuffer[Msg]()
-
-      msg.obj.foreach {
-        case (Str(k), v) if rhsFields.contains(k) =>
-          v match {
-            case Arr(vs) =>
-              vs.foreach { inner =>
-                val base = upack.copy(inner)
-                base.obj.update(Str(rhsKey), lhsId)
-                rhsOut.append(base)
-              }
-            case single =>
-              val base = upack.copy(single)
-              base.obj.update(Str(rhsKey), lhsId)
-              rhsOut.append(base)
-          }
-        case (k, v) => lhsOut.update(k, v)
-      }
-
-      ctx.output(sideOut, Obj(lhsOut))
-      rhsOut
-    }
-
-    (sideOuts(sideOut), rhs)
+    (
+      trimmedVcvStream,
+      sideCtx(rcvOut),
+      sideCtx(variationOut),
+      sideCtx(scvOut),
+      sideCtx(scvVariationOut)
+    )
   }
 }
