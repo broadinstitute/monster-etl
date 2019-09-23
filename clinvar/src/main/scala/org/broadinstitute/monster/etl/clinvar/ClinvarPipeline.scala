c661d08454593c4814205a3eaeffe8face755290
Dan Moran <danmoran@broadinstitute.org>
2019-09-23 15:26:31 -0400

    Various tweaks.

--- clinvar/src/main/scala/org/broadinstitute/monster/etl/clinvar/ClinvarPipeline.scala
+++ clinvar/src/main/scala/org/broadinstitute/monster/etl/clinvar/ClinvarPipeline.scala
@@ -1,5 +1,7 @@
 package org.broadinstitute.monster.etl.clinvar
 
+import java.util.concurrent.atomic.AtomicInteger
+
 import caseapp.{AppName, AppVersion, HelpMessage, ProgName}
 import cats.data.NonEmptyList
 import com.spotify.scio.ContextAndArgs
@@ -84,12 +86,12 @@ object ClinvarPipeline {
     val scvs = rawScvs.map(
       mapFields(
         Map(
-          NonEmptyList.of("@ID") -> "id",
           NonEmptyList.of("@DateCreated") -> "date_created",
           NonEmptyList.of("@DateLastUpdated") -> "date_last_updated",
           NonEmptyList.of("RecordStatus") -> "record_status",
           NonEmptyList.of("ReviewStatus") -> "review_status",
           NonEmptyList.of("@SubmissionDate") -> "submission_date",
+          NonEmptyList.of("SubmissionNameList", "SubmissionName") -> "submission_names",
           NonEmptyList.of("ClinVarAccession", "@Accession") -> "accession",
           NonEmptyList.of("ClinVarAccession", "@Version") -> "version",
           NonEmptyList.of("ClinVarAccession", "@Type") -> "assertion_type",
@@ -110,59 +112,10 @@ object ClinvarPipeline {
       )
     )
     val scvVariations = rawScvVariations.map(
-      mapFields(
-        Map(
-          NonEmptyList.of("@ID") -> "id",
-          NonEmptyList.of("VariantType") -> "variation_type"
-        )
-      )
+      mapFields(Map(NonEmptyList.of("VariantType") -> "variation_type"))
     )
 
-    val (clinicalAssertions, submitters, submissions) = {
-      val submitterOut = SideOutput[Msg]
-      val submissionOut = SideOutput[Msg]
-
-      val (main, side) = scvs.withSideOutputs(submitterOut, submissionOut).map {
-        (scv, ctx) =>
-          val scvObj = scv.obj
-          val newScv = new mutable.LinkedHashMap[Msg, Msg]()
-
-          val scvId = scvObj(idKey)
-          val orgId = scvObj(Str("org_id"))
-          val submitDate = scvObj(Str("submission_date"))
-          val submissionId = s"${orgId.str}.${submitDate.str}"
-
-          val submitter = new mutable.LinkedHashMap[Msg, Msg]()
-          val submission = new mutable.LinkedHashMap[Msg, Msg]()
-
-          // Set and link IDs
-          newScv.update(idKey, scvId)
-          submitter.update(idKey, orgId)
-          submission.update(idKey, Str(submissionId))
-          submitter.update(scvRef, scvId)
-          submission.update(scvRef, scvId)
-          submission.update(Str("submitter_id"), orgId)
-
-          // Fill in other properties
-          val submitterFields =
-            Set("submitter_name", "org_category", "org_abbrev").map(Str)
-          val droppedFields = Set("org_id", "submissionDate").map(Str)
-          (scvObj.keySet -- submitterFields -- droppedFields).foreach { k =>
-            scvObj.get(k).foreach(newScv.update(k, _))
-          }
-          submitterFields.foreach { k =>
-            scvObj.get(k).foreach(submitter.update(k, _))
-          }
-          submission.update(Str("submission_date"), submitDate)
-
-          // Push outputs.
-          ctx.output(submitterOut, Obj(submitter): Msg)
-          ctx.output(submissionOut, Obj(submission): Msg)
-          Obj(newScv): Msg
-      }
-
-      (main, side(submitterOut), side(submissionOut))
-    }
+    val (clinicalAssertions, submitters, submissions) = splitScvs(scvs)
 
     MsgIO.writeJsonLists(
       vcvs,
@@ -205,12 +158,15 @@ object ClinvarPipeline {
   }
 
   val idKey = Str("id")
-  val vcvRef = Str("variation_archive_id")
-  val rcvRef = Str("rcv_accession_ids")
+  val vcvRef = Str("variation_archive_accession")
+  val rcvRef = Str("rcv_accessions")
   val varRef = Str("variation_archive_variation_id")
   val parentVarRef = Str("parent_id")
   val parentVarsRef = Str("parent_ids")
-  val scvRef = Str("clinical_assertion_id")
+  val scvRef = Str("clinical_assertion_accession")
+  val submitterRef = Str("submitter_id")
+  val submissionRef = Str("submission_id")
+  val subclassKey = Str("subclass_type")
 
   val generatedKeys = Set[Msg](
     idKey,
@@ -219,18 +175,57 @@ object ClinvarPipeline {
     varRef,
     parentVarRef,
     parentVarsRef,
-    scvRef
+    scvRef,
+    submitterRef,
+    submissionRef,
+    subclassKey
   )
 
   val interpretedRecord = Str("InterpretedRecord")
   val includedRecord = Str("IncludedRecord")
 
   def getVariant(message: Msg): Option[Msg] = {
-    message.obj
-      .remove(Str("SimpleAllele"))
-      .orElse(message.obj.remove(Str("Haplotype")))
-      .orElse(message.obj.remove(Str("Genotype")))
+    val subtypes = List("SimpleAllele", "Haplotype", "Genotype").map(Str)
+    subtypes.foldLeft(Option.empty[Msg]) { (acc, subtype) =>
+      acc.orElse {
+        message.obj.remove(subtype).map {
+          case arr @ Arr(msgs) =>
+            msgs.foreach(_.obj.update(subclassKey, subtype))
+            arr
+          case msg =>
+            msg.obj.update(subclassKey, subtype)
+            msg
+        }
+      }
+    }
   }
+
+  def unrollVariants(
+    variantWrapper: Msg,
+    parentIds: List[Msg],
+    getId: Msg => Msg,
+    output: Msg => Unit
+  ): Iterable[Msg] =
+    getVariant(variantWrapper).toIterable.flatMap {
+      case Arr(vs) => vs
+      case other   => Iterable(other)
+    }.map { variant =>
+      val variantId = getId(variant)
+      val immediateParentId = parentIds.headOption
+
+      // Unroll any children.
+      val _ = unrollVariants(variant, variantId :: parentIds, getId, output)
+
+      // Link upwards.
+      variant.obj.update(parentVarsRef, Arr(parentIds.to[mutable.ArrayBuffer]))
+      immediateParentId.foreach(variant.obj.update(parentVarRef, _))
+
+      // Output this fully-unrolled variant.
+      output(variant)
+
+      // Return this ID, in case the caller cares.
+      variantId
+    }
 
   def splitArchives(fullVcvStream: SCollection[Msg]): (
     SCollection[Msg],
@@ -255,10 +250,8 @@ object ClinvarPipeline {
       .map { (fullVcv, ctx) =>
         // Init top-level VCV info.
         val vcvObj = fullVcv.obj
-        val vcvId =
-          Str(s"${vcvObj(Str("@Accession")).str}.${vcvObj(Str("@Version")).str}")
+        val vcvId = vcvObj(Str("@Accession"))
         val trimmedOut = new mutable.LinkedHashMap[Msg, Msg]
-        trimmedOut.update(idKey, vcvId)
 
         // Pull out top-level record.
         // Fields which are destined for other tables will be stripped away.
@@ -268,58 +261,48 @@ object ClinvarPipeline {
           val recordCopy = upack.copy(originalRecord)
 
           // Extract and push out any RCVs in the VCV, tracking the collected IDs.
-          val rcvIds = new mutable.ArrayBuffer[Msg]()
-          val rcvs = for {
-            wrapper <- recordCopy.obj.remove(Str("RCVList"))
-            arrayOrSingle <- wrapper.obj.get(Str("RCVAccession"))
-          } yield {
-            arrayOrSingle match {
-              case Arr(msgs) => msgs
-              case msg       => Iterable(msg)
+          val rcvIds = {
+            val idBuffer = new mutable.ArrayBuffer[Msg]()
+            val rcvs = for {
+              wrapper <- recordCopy.obj.remove(Str("RCVList"))
+              arrayOrSingle <- wrapper.obj.get(Str("RCVAccession"))
+            } yield {
+              arrayOrSingle match {
+                case Arr(msgs) => msgs
+                case msg       => Iterable(msg)
+              }
             }
+            rcvs.getOrElse(Iterable.empty).foreach { rcv =>
+              val rcvObj = rcv.obj
+
+              // Generate and track RCV ID.
+              val id = rcvObj(Str("@Accession"))
+              idBuffer.append(id)
+
+              // Link back to VCV.
+              rcvObj.update(vcvRef, vcvId)
+
+              // Push to side output.
+              ctx.output(rcvOut, rcv)
+            }
+
+            Arr(idBuffer)
           }
-          rcvs.getOrElse(Iterable.empty).foreach { rcv =>
-            val rcvObj = rcv.obj
 
-            // Generate and track RCV ID.
-            val id =
-              Str(s"${rcvObj(Str("@Accession")).str}.${rcvObj(Str("@Version")).str}")
-            rcvObj.update(idKey, id)
-            rcvIds.append(id)
-
-            // Link back to VCV.
-            rcvObj.update(vcvRef, vcvId)
-
-            // Push to side output.
-            ctx.output(rcvOut, rcv)
-          }
-
-          // Extract and push archive-level variation records.
-          // Variations are still nested when pushed out here.
-          def unrollVariants(variantWrapper: Msg, parentIds: List[Msg]): Iterable[Msg] =
-            getVariant(variantWrapper).toIterable.flatMap {
-              case Arr(vs) => vs
-              case other   => Iterable(other)
-            }.map { variant =>
-              val variantId = variant.obj(Str("@VariationID"))
-              val immediateParentId = parentIds.headOption
-
-              // Link upwards.
+          // Extract and push top-level variant info from the VCV.
+          val variationId = unrollVariants(
+            recordCopy,
+            Nil,
+            _.obj(Str("@VariationID")),
+            variant => {
+              // Link back to the VCV and RCVs.
               variant.obj.update(vcvRef, vcvId)
-              variant.obj.update(rcvRef, Arr(rcvIds))
-              variant.obj.update(parentVarRef, Arr(parentIds.to[mutable.ArrayBuffer]))
-              immediateParentId.foreach(variant.obj.update(parentVarsRef, _))
-
-              // Continue unrolling.
-              val _ = unrollVariants(variant, variantId :: parentIds)
-
-              // Output this fully-unrolled variant.
+              variant.obj.update(rcvRef, rcvIds)
+              // Push to side output.
               ctx.output(variationOut, variant)
-
-              variantId
+              ()
             }
-
-          val variationId = unrollVariants(recordCopy, Nil).headOption
+          )
 
           // Extract and push out any SCVs and SCV-level variations.
           val scvs = for {
@@ -333,21 +316,31 @@ object ClinvarPipeline {
           }
           scvs.getOrElse(Iterable.empty).foreach { scv =>
             val scvObj = scv.obj
-            val scvId = scvObj(Str("@ID"))
+            val scvId = scvObj(Str("ClinVarAccession")).obj(Str("@Accession"))
 
             // Link the SCV back to the VCV, RCVs, and top-level variant.
             scvObj.update(vcvRef, vcvId)
-            scvObj.update(rcvRef, Arr(rcvIds))
+            scvObj.update(rcvRef, rcvIds)
             variationId.foreach(scvObj.update(varRef, _))
 
             // Extract out any SCV-level variation.
-            val variation = getVariant(scv)
-            variation.foreach { variant =>
-              // Link the variation back to its SCV.
-              variant.obj.update(scvRef, scvId)
-              // Push to side output.
-              ctx.output(scvVariationOut, variant)
-            }
+            val counter = new AtomicInteger
+            val _ = unrollVariants(
+              scv,
+              Nil,
+              variant => {
+                val nextId = Str(s"${scvId.str}.${counter.getAndIncrement()}")
+                variant.obj.update(idKey, nextId)
+                nextId
+              },
+              variant => {
+                // Link the variation back to its SCV.
+                variant.obj.update(scvRef, scvId)
+                // Push to side output.
+                ctx.output(scvVariationOut, variant)
+                ()
+              }
+            )
 
             // Push SCV to side output.
             ctx.output(scvOut, scv)
@@ -356,6 +349,7 @@ object ClinvarPipeline {
           recordCopy
         }
 
+        // TODO useful comment
         vcvObj.foreach {
           case (k, v) =>
             if (k == interpretedRecord || k == includedRecord) {
@@ -365,6 +359,7 @@ object ClinvarPipeline {
             }
         }
 
+        // Whatever's left in the VCV object is returned as the main output.
         Obj(trimmedOut): Msg
       }
 
@@ -415,5 +410,61 @@ object ClinvarPipeline {
     out.update(Str("content"), Str(stringContent.toString))
 
     Obj(out): Msg
+  }
+
+  def splitScvs(
+    scvs: SCollection[Msg]
+  ): (SCollection[Msg], SCollection[Msg], SCollection[Msg]) = {
+    val submitterOut = SideOutput[Msg]
+    val submissionOut = SideOutput[Msg]
+
+    val (main, side) = scvs.withSideOutputs(submitterOut, submissionOut).map {
+      (scv, ctx) =>
+        val scvObj = scv.obj
+        val newScv = new mutable.LinkedHashMap[Msg, Msg]()
+
+        val scvId = scvObj(Str("accession"))
+        val orgId = scvObj(Str("org_id"))
+        val submitDate = scvObj(Str("submission_date"))
+        val submissionId = Str(s"${orgId.str}.${submitDate.str}")
+
+        val submitter = new mutable.LinkedHashMap[Msg, Msg]()
+        val submission = new mutable.LinkedHashMap[Msg, Msg]()
+
+        // Set and link IDs
+        submitter.update(idKey, orgId)
+        submission.update(idKey, submissionId)
+        submission.update(submitterRef, orgId)
+        newScv.update(idKey, scvId)
+        newScv.update(submitterRef, orgId)
+        newScv.update(submissionRef, submissionId)
+
+        // Fill in other properties
+        val submitterFields =
+          Set("submitter_name", "org_category", "org_abbrev").map(Str)
+        val submissionFields =
+          Set("submission_date", "submission_names").map(Str)
+        (scvObj.keySet -- submitterFields -- submissionFields - Str("org_id")).foreach {
+          k =>
+            scvObj.get(k).foreach(newScv.update(k, _))
+        }
+        submitterFields.foreach { k =>
+          scvObj.get(k).foreach(submitter.update(k, _))
+        }
+        submissionFields.foreach { k =>
+          scvObj.get(k).foreach(submission.update(k, _))
+        }
+
+        // Push outputs.
+        ctx.output(submitterOut, Obj(submitter): Msg)
+        ctx.output(submissionOut, Obj(submission): Msg)
+        Obj(newScv): Msg
+    }
+
+    (
+      main,
+      side(submitterOut).distinctBy(_.obj(idKey).str),
+      side(submissionOut).distinctBy(_.obj(idKey).str)
+    )
   }
 }
