cf5c2ee3e2a8c5788fa36956abec3f8e896dc934
Dan Moran <danmoran@broadinstitute.org>
2019-11-14 09:50:38 -0500

    DSPDC-612 Cross-link SCV traits to VCV traits using mappings.

--- clinvar/src/main/scala/org/broadinstitute/monster/etl/clinvar/models/output/SCVTrait.scala
+++ clinvar/src/main/scala/org/broadinstitute/monster/etl/clinvar/models/output/SCVTrait.scala
@@ -27,7 +27,8 @@ case class SCVTrait(
   medgenId: Option[String],
   name: Option[String],
   `type`: Option[String],
-  xrefs: Array[String]
+  xrefs: Array[String],
+  traitId: Option[String]
 )
 
 object SCVTrait {
@@ -38,13 +39,16 @@ object SCVTrait {
   /** Extract an SCVTrait from a raw Trait payload. */
   def fromRawTrait(
     traitSet: SCVTraitSet,
+    traits: Array[VCVTrait],
+    traitMappings: Array[TraitMapping],
     counter: AtomicInteger,
     rawTrait: Msg
   ): SCVTrait = {
     val nameWrapper = rawTrait.extract("Name", "ElementValue")
+    val nameType = rawTrait.extract("@Type").map(_.str)
     val allXrefs = MsgTransformations.popAsArray(rawTrait, "XRef")
     val (medgenId, xrefs) =
-      allXrefs.foldLeft((Option.empty[String], List.empty[String])) {
+      allXrefs.foldLeft((Option.empty[String], List.empty[Msg])) {
         case ((medgenAcc, xrefAcc), xref) =>
           val db = xref.obj.get(Str("@DB")).map(_.str)
           val id = xref.obj.get(Str("@ID")).map(_.str)
@@ -65,19 +69,59 @@ object SCVTrait {
                   MsgTransformations.keyToSnakeCase(k.str.replaceAllLiterally("@", ""))
                 cleaned.obj.update(Str(cleanedKey), v)
             }
-            val stringifiedRef = upack.transform(cleaned, StringRenderer()).toString
 
-            (medgenAcc, stringifiedRef :: xrefAcc)
+            (medgenAcc, cleaned :: xrefAcc)
           }
       }
 
-    SCVTrait(
+    val baseScv = SCVTrait(
       id = s"${traitSet.id}.${counter.getAndIncrement()}",
       clinicalAssertionTraitSetId = traitSet.id,
       medgenId = medgenId,
       name = nameWrapper.map(_.value.str),
       `type` = rawTrait.extract("@Type").map(_.str),
-      xrefs = xrefs.toArray
+      xrefs = xrefs.toArray.map(upack.transform(_, StringRenderer()).toString),
+      traitId = None
     )
+
+    if (traitMappings.isEmpty) {
+      baseScv.copy(traitId = traits.headOption.map(_.id))
+    } else {
+      // Find the MedGen ID to look for in the VCV traits.
+      val matchingMedgenId = baseScv.medgenId.orElse {
+        traitMappings.find { candidateMapping =>
+          val sameTraitType =
+            baseScv.`type`.contains(candidateMapping.traitType)
+
+          val nameMatch = {
+            val isNameMapping = candidateMapping.mappingType == "Name"
+            val isPreferredMatch = candidateMapping.mappingRef == "Preferred" &&
+              nameType.contains("Preferred") &&
+              baseScv.name.contains(candidateMapping.mappingValue)
+            val isAlternateMatch = candidateMapping.mappingRef == "Alternate" &&
+              nameType.contains("included") &&
+              baseScv.name.contains(candidateMapping.mappingValue)
+
+            isNameMapping && (isPreferredMatch || isAlternateMatch)
+          }
+
+          val xrefMatch = {
+            val isXrefMapping = candidateMapping.mappingType == "XRef"
+            val xrefMatches = xrefs.exists { xref =>
+              xref.obj(Str("db")).str == candidateMapping.mappingRef &&
+              xref.obj(Str("id")).str == candidateMapping.mappingValue
+            }
+
+            isXrefMapping && xrefMatches
+          }
+
+          sameTraitType && (nameMatch || xrefMatch)
+        }.flatMap(_.medgenId)
+      }
+      // Find the VCV trait with the MedGen ID.
+      val matchingVcvTrait = traits.find(_.medgenId == matchingMedgenId)
+      // Link!
+      baseScv.copy(traitId = matchingVcvTrait.map(_.id))
+    }
   }
 }
