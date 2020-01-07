1647e738b8d63d947992e9b6cfb3e8637f64b26d
Raaid Arshad <rarshad@broadinstitute.org>
2020-01-07 17:25:16 -0500

    refactor argument, add scaladocs

--- clinvar/src/main/scala/org/broadinstitute/monster/etl/clinvar/models/output/XRef.scala
+++ clinvar/src/main/scala/org/broadinstitute/monster/etl/clinvar/models/output/XRef.scala
@@ -4,6 +4,13 @@ import io.circe.{Encoder, Json}
 import io.circe.derivation.deriveEncoder
 import upack.{Msg, Str}
 
+/**
+  * Info about an XRef (cross-reference) in a trait.
+  *
+  * @param db the database it is from
+  * @param id the id of it in the aforementioned database database
+  * @param sourceName a human sensible name for it, if one is provided
+  */
 case class XRef(db: String, id: String, sourceName: Option[String])
 
 object XRef {
@@ -12,7 +19,14 @@ object XRef {
   implicit val encoder: Encoder[XRef] =
     deriveEncoder.mapJson(obj => Json.fromString(obj.noSpaces))
 
-  def fromRawXRef(rawXref: Msg, nameValue: Option[String] = Option.empty): XRef = {
+  /**
+    * Extract an XRef from a rawXref and combine it with a name (if one is provided).
+    *
+    * @param rawXref a Msg containing the db and id information of the XRef
+    * @param referencedName the human sensible name for the XRef, if one is provided
+    * @return
+    */
+  def fromRawXRef(rawXref: Msg, referencedName: Option[String] = Option.empty): XRef = {
     XRef(
       rawXref.obj
         .getOrElse(
@@ -26,7 +40,7 @@ object XRef {
           throw new IllegalStateException(s"No ID tag found in xref: $rawXref")
         )
         .str,
-      nameValue
+      referencedName
     )
   }
 }
